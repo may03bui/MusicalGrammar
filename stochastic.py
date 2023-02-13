9d26cd11da7b3a16dacc913c9874ae6a8832fb8c
May <mb2450@cam.ac.uk>
2023-02-13 14:51:26 +0000

    Reformatted stochastic.py

--- stochastic.py
+++ stochastic.py
@@ -3,33 +3,43 @@ from mingus.containers import Track
 import numpy as np
 
 
-def main():
-    song = "EDCDEEEDDDEGGEDCDEEEEDDEDC"
-    alphabet = ["C", "D", "E", "G"]
+def get_transition_probs(string, alphabet):
     t_probs = {n1: {n2: 0 for n2 in alphabet} for n1 in alphabet}
-    t_probs["C"]["E"] = 1    # Wrap the melody back round
 
-    for i in range(len(song)-1):
-        row = t_probs[song[i]]
-        row[song[i+1]] = row.get(song[i+1]) + 1
-    
+    for i in range(len(string) - 1):
+        row = t_probs[string[i]]
+        row[string[i + 1]] = row.get(string[i + 1]) + 1
+
     for n, row in t_probs.items():
-        t_probs[n] = {k: v/sum(row.values()) for k, v in row.items()}
+        print(row.items())
+        t_probs[n] = {k: v / sum(row.values()) for k, v in row.items()}
 
-    current_note = np.random.choice(alphabet, p=[song.count(n)/len(song) for n in alphabet])
-    melody = current_note
+    return t_probs
+
+
+def generate_melody(starting_note, t_probs, alphabet, iters):
+    melody = starting_note
     t = Track()
-    t.add_notes(current_note)
+    t.add_notes(melody)
 
-    for _ in range(15):
+    for _ in range(iters):
         prev = melody[-1]
-        current_note = np.random.choice(alphabet, p=[t_probs[prev][n] for n in alphabet]) 
+        print(t_probs)
+        current_note = np.random.choice(alphabet, p=[t_probs[prev][n] for n in alphabet])
         melody += current_note
         t.add_notes(current_note)
 
-    print(melody)
+    return t, melody
+
+
+def main():
+    alphabet = ["C", "D", "E", "G"]
+    song = "EDCDEEEDDDEGGEDCDEEEEDDEDCC"  # Added C to wrap the melody back around
+    t_probs = get_transition_probs(song, alphabet)
+    track, melody = generate_melody("C", t_probs, alphabet, 15)
+
     fluidsynth.init("FluidR3_GM.sf2")
-    fluidsynth.play_Track(t, 1, 160)
+    fluidsynth.play_Track(track, 1, 160)
 
 
 if __name__ == "__main__":
